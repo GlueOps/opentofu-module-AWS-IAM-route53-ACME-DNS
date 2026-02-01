4d8bb387507de8a0e02e24e291b6348a5f6a1116
Venkat <venkata@venkatamutyala.com>
2026-02-01 02:16:04 +0000

    fix: provider requirements

--- versions.tf
+++ versions.tf
@@ -3,9 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 4.0"
-      configuration_aliases = [aws]
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
   }
 }
